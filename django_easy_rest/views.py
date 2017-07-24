8893a80ca0dcd99e823ad00afc98e032c69a2a4f
jonatan <jonatanshimon@gmail.com>
2017-07-24 11:20:33 +0300

    more debug info and api abstractions

--- django_easy_rest/views.py
+++ django_easy_rest/views.py
@@ -18,10 +18,7 @@ class MethodApiView(APIView):
     method_helpers = {'__all__': {"help": {"general": "this is a special message"}}}
     api_allowed_methods = ['__all__']
 
-    # this field is responsible of handling model get in functions
-    api_handle_models_get = True
-    # unpack api data into function
-    api_method_unpackers = True
+    api_abstraction_methods = ['__all__']
 
     # model resolver
 
@@ -37,22 +34,33 @@ class MethodApiView(APIView):
     def _pythonize(name):
         return name.replace('-', '_').lower()
 
+    def restifiy(self, data):
+        if ' ' in data:
+            return data.replace(' ', self.separator)
+        if '_' in data:
+            return data.replace('_', self.separator)
+        if '-' in data:
+            return data.replace('-', self.separator)
+
     # {"action":"error"}
     def post(self, request):
         base_response = {}
         if settings.DEBUG:
-            base_response['debug'] = {}
+            base_response['debug'] = {self.restifiy("api attributes"):
+                                          {self.restifiy("api allowed methods"): self.api_allowed_methods,
+                                           self.restifiy("api abstraction methods"): self.api_abstraction_methods}}
         try:
             data, debug_data = self.api_abstractions(request.data)
             if settings.DEBUG:
-                base_response = {"debug": {"processed-data": debug_data}}
+                base_response["debug"]["processed-data"] = debug_data
             if self.function_field_name in data:
                 action = self._pythonize(data[self.function_field_name])
                 try:
                     if action not in self.api_allowed_methods and '__all__' not in self.api_allowed_methods:
-                        base_response['error'] = '{0} {1} not allowed , allowed {0} {2}'.format(self.function_field_name,
-                                                                                               action,
-                                                                                               self.api_allowed_methods)
+                        base_response['error'] = '{0} {1} not allowed , allowed {0} {2}'.format(
+                            self.function_field_name,
+                            action,
+                            self.api_allowed_methods)
                         return Response(data=base_response,
                                         status=status.HTTP_403_FORBIDDEN)
                     _method = getattr(self, action)
@@ -108,7 +116,7 @@ class MethodApiView(APIView):
         prepared_data = {}
         if append_data is None:
             append_data = {}
-        if not method or not self.api_method_unpackers:
+        if not method:
             prepared_data = {"data": data}
         else:
             keys = list(method.__code__.co_varnames)
@@ -127,39 +135,31 @@ class MethodApiView(APIView):
         return prepared_data
 
     def api_abstractions(self, data):
-        get_model = 'get{separator}model'.format(separator=self.separator)
         debug_data = copy(data)
-        if get_model in data:
-            '''
-            the get model is as follows:
-            
-            {"get-model": {"model-name":"User", "query":{"pk":5}, "app":"auth"}}
-            
-            or 
-            
-            {"get-model": {"model-name":"auth.User", "query":{"pk":5}}}
-            
-            for many use 
-           {"action":"correct", "get-model": [{"field":"auth.User", "query":{"pk":1}},{"field":"auth.User", "query":{"pk":1}}]}
-            default parameter name is lower of model name
-            
-            demo:
-            {"action":"correct", "get-model": [{"field":"auth.User", "query":{"pk":1}},{"field":"auth.User", "query":{"pk":1}}]}
-            
-            '''
-            if type(data[get_model]) is not list:
-                data[get_model] = [data[get_model]]
-            for i in range(len(data[get_model])):
-                obj, debug_obj = self.get_model(**data[get_model][i])
-                prm_key = list(obj.keys())[0]
-                if prm_key in data:
-                    data[prm_key] = [data[prm_key], obj[prm_key]]
-                    debug_data[prm_key] = [debug_data[prm_key], debug_obj[prm_key]]
-                else:
-                    data.update(obj)
-                    debug_data.update(debug_obj)
-            del data[get_model]
-            del debug_data[get_model]
+        check = lambda item: True
+        if '__all__' not in self.api_abstraction_methods:
+            check = lambda item: self._pythonize(item) in self.api_abstraction_methods
+
+        if check("get model"):
+            get_model_key = self.restifiy('get model')
+            if get_model_key in data:
+                data, debug_data = self.handle_get_model(data=data, get_model=get_model_key, debug_data=debug_data)
+        return data, debug_data
+
+    def handle_get_model(self, data, get_model, debug_data):
+        if type(data[get_model]) is not list:
+            data[get_model] = [data[get_model]]
+        for i in range(len(data[get_model])):
+            obj, debug_obj = self.get_model(**data[get_model][i])
+            prm_key = list(obj.keys())[0]
+            if prm_key in data:
+                data[prm_key] = [data[prm_key], obj[prm_key]]
+                debug_data[prm_key] = [debug_data[prm_key], debug_obj[prm_key]]
+            else:
+                data.update(obj)
+                debug_data.update(debug_obj)
+        del data[get_model]
+        del debug_data[get_model]
         return data, debug_data
 
     def get_model(self, query, field=None, model_name=None, app=None, split_by='.'):
