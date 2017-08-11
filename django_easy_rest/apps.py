baaae35c5a399d130a468e89afbd14e0656d9ecb
jonatan <jonatanshimon@gmail.com>
2017-08-11 15:00:00 +0300

    more comments

--- django_easy_rest/apps.py
+++ django_easy_rest/apps.py
@@ -1,5 +1,30 @@
 from django.apps import AppConfig
+import sys
+
+
+class ValidateLoading(object):
+    dependencies = [
+        'rest_framework',
+    ]
+
+    def validate_dependencies(self):
+        uninstalled = []
+        for dependency in self.dependencies:
+            try:
+                __import__(dependency)
+            except ImportError:
+                uninstalled += [dependency]
+        if uninstalled:
+            return ('required dependencies {}, consider installing them by: '
+                    'pip').format(','.join(uninstalled))
+
+    def validate(self):
+        error = self.validate_dependencies()
+        if error:
+            print(error)
+            sys.exit(-1)
 
 
 class DjangoEasyRestConfig(AppConfig):
+    ValidateLoading().validate()
     name = 'django_easy_rest'
