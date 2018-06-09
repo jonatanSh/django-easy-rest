92f94ff8cea96160101e896e8d81aab6d500df06
Jonathan <jonatanshimon@gmail.com>
2018-06-09 22:14:29 +0300

    version bump (fixes, and get requests)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (ImportError, OSError):
     README = ""
 setup(
     name='django-easy-rest',
-    version='2.2',
+    version='2.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['djangorestframework', 'django'],
